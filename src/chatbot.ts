5408786d444c044a9987ec163643ac096898e1b8
ArturM-S <silvamarquesartur@gmail.com>
2022-10-25 18:09:53 -0300

    Chore: more bots

--- src/chatbot.ts
+++ src/chatbot.ts
@@ -23,33 +23,95 @@ client.on('ready', () => {
 // create a function that go to url and get screenshot of the page 
 
 
-client.on('message_create', (message: Message) => {
-	console.log(message.body);
-    if(message.body === '!screenshot') {
+client.on('message_create', async (message: Message) => {
+    if(message.body.startsWith('!noticia')) {
 		async function getScreenshot() {
             const browser = await client.pupBrowser
             if(!browser) return
             const page = await browser.newPage();
-            await page.goto('https://www.w3schools.com/jsref/met_win_print.asp');
-            // get screenshot ins base64 as string
-        
-            const image = await page.screenshot({ encoding: 'base64' });
+            await page.goto(`https://www.google.com/search?q=${message.body.replace('!noticia', '')}&tbm=nws`);
+            await page.click('div.mCBkyc.y355M.ynAwRc.MBeuO.nDgy9d');
+            await page.waitForTimeout(7000);
+            const image = await page.screenshot({ encoding: 'base64', type: 'jpeg', quality: 100, clip: { x: 0, y: 0, width: 1250, height: 1250 } });
             await page.close();
             return image;
-            console.log(image);
         }
         
         // create a function that send screenshot to whatsapp
         async function sendScreenshot() {
             const image = await getScreenshot() as string;
 
-            const media = new MessageMedia('image/png', image, 'screenshot.png');
+            const media = new MessageMedia('image/jpeg', image);
 
             await message.reply( media);
         }
         sendScreenshot()
         console.log('screenshot sent')
 	}
+    if(message.body.startsWith('!pesquisa')) {
+		async function getScreenshot() {
+            const browser = await client.pupBrowser
+            if(!browser) return
+            const page = await browser.newPage();
+            await page.goto(`https://www.google.com/search?q=${message.body.replace('!pesquisa', '')}`);
+            await page.click('h3');
+            await page.waitForTimeout(7000);
+            const image = await page.screenshot({ encoding: 'base64', type: 'jpeg', quality: 100, clip: { x: 0, y: 0, width: 1250, height: 1250 } });
+            await page.close();
+            return image;
+        }
+        
+        // create a function that send screenshot to whatsapp
+        async function sendScreenshot() {
+            const image = await getScreenshot() as string;
+
+            const media = new MessageMedia('image/jpeg', image);
+
+            await message.reply( media);
+        }
+        sendScreenshot()
+        console.log('screenshot sent')
+	}
+    if(message.body.startsWith('!imagem')) {
+		async function getScreenshot() {
+            const browser = await client.pupBrowser
+            if(!browser) return
+            const page = await browser.newPage();
+            await page.goto(`https://www.google.com/search?q=${message.body.replace('!imagem', '')}&tbm=isch`);
+            await page.click('img.rg_i.Q4LuWd',{ button: "middle"});
+            let pages = await browser.pages();
+
+            await page.waitForTimeout(7000);
+            const image = await page.screenshot({ encoding: 'base64', type: 'jpeg', quality: 100, clip: { x: 0, y: 0, width: 1250, height: 1250 } });
+            await page.close();
+            return image;
+        }
+        
+        // create a function that send screenshot to whatsapp
+        async function sendScreenshot() {
+            const image = await getScreenshot() as string;
+
+            const media = new MessageMedia('image/jpeg', image);
+
+            await message.reply( media);
+        }
+        sendScreenshot()
+        console.log('screenshot sent')
+	}
+    if(message.body === '!sticker') {
+        if(message.hasMedia){
+            const media = await message.downloadMedia();
+            message.reply(media, undefined, {sendMediaAsSticker: true});
+        }
+        else{
+            const quotedMessage = await message.getQuotedMessage();
+            if(quotedMessage.hasMedia){
+                const media = await quotedMessage.downloadMedia();
+                message.reply(media, undefined, {sendMediaAsSticker: true});
+            }
+        }
+
+    }
 });
 
 
